2c65b09b4f10025bb575ef7b6aa2b7e5ffbf7bb0
Rene Cunningham <rene@compounddata.com>
2023-12-28 06:49:06 +1100

    implement queue for transaction processing

--- cmd/transactionsearch/main.go
+++ cmd/transactionsearch/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
 	"transactionsearch/db/migrations"
 	"transactionsearch/internal/router"
+	"transactionsearch/internal/transaction"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -37,6 +43,16 @@ func main() {
 		doMigrate = true
 	}
 
+	_workerQueueLength, ok := os.LookupEnv("TS_WORKER_QUEUE_LENGTH")
+	if !ok {
+		_workerQueueLength = "2"
+	}
+
+	workerQueueLength, err := strconv.Atoi(_workerQueueLength)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	db, err := sql.Open("pgx", postgresURI)
 	if err != nil {
 		log.Fatal(err)
@@ -53,10 +69,83 @@ func main() {
 		return
 	}
 
-	rtr := router.NewRouter(db)
-	listenAddrPort := fmt.Sprintf("%s:%s", httpHost, httpPort)
-	log.Printf("Server listening on http://%s", listenAddrPort)
-	if err := http.ListenAndServe(listenAddrPort, rtr); err != nil {
-		log.Fatalf("%v", err)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func() {
+		cancel()
+	}()
+
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, os.Interrupt, syscall.SIGTERM)
+
+	workerCh := make(chan router.WorkerRequest, workerQueueLength)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		rtr := router.NewRouter(db, workerCh)
+		listenAddrPort := fmt.Sprintf("%s:%s", httpHost, httpPort)
+		s := &http.Server{
+			Addr:    listenAddrPort,
+			Handler: rtr,
+		}
+
+		go func() {
+			log.Printf("Server listening on http://%s", listenAddrPort)
+			if err := s.ListenAndServe(); err != http.ErrServerClosed {
+				panic(err)
+			}
+		}()
+
+		for {
+			select {
+			case <-ctx.Done():
+				log.Print("router shutting down")
+				return s.Shutdown(ctx)
+			}
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		for {
+			select {
+			case <-sigC:
+				close(workerCh)
+				return fmt.Errorf("received SIGTERM/SIGINT, exiting")
+			case <-ctx.Done():
+				close(workerCh)
+				return nil
+			}
+		}
+	})
+
+	g.Go(func() error {
+		for req := range workerCh {
+			resp := router.WorkerResponse{Error: nil}
+
+			transactionHandlers := []transaction.TransactionHandler{
+				transaction.NewTransactionState(),
+				transaction.NewTransactionPostcode(),
+				transaction.NewTransactionOrganisation(),
+			}
+
+			for _, handler := range transactionHandlers {
+				if err := handler.Handle(ctx, db, req.Transaction); err != nil {
+					resp.Error = err
+					break
+				}
+			}
+
+			resp.Transaction = req.Transaction
+			req.Chan <- resp
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Printf("errgroup received error: %v", err)
+		cancel()
+		os.Exit(1)
 	}
+
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -2,16 +2,17 @@ package router
 
 import (
 	"database/sql"
+	"transactionsearch/internal/transaction"
 
 	"github.com/gin-gonic/gin"
 )
 
-func NewRouter(db *sql.DB) *gin.Engine {
+func NewRouter(db *sql.DB, ch chan WorkerRequest) *gin.Engine {
 	r := gin.Default()
 	r.Static("/static", "./static")
 	r.GET("/about", aboutHandler())
 	r.GET("/", homeHandler())
-	r.POST("/transactions", transactionHandler(db))
+	r.POST("/transactions", transactionHandler(db, ch))
 	return r
 }
 
@@ -22,4 +23,14 @@ type ViewData struct {
 
 func NewViewData(currentPage string) ViewData {
 	return ViewData{CurrentPage: currentPage}
+}
+
+type WorkerResponse struct {
+	Transaction *transaction.Transaction
+	Error       error
+}
+
+type WorkerRequest struct {
+	Chan        chan WorkerResponse
+	Transaction *transaction.Transaction
 }
